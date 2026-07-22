2931ea563db1437bb894b58fdcb96988187cf527
Claudinei Machado <cjchamado@gmail.com>
2026-07-22 20:27:07 -0300

    seeding order + order_items

---
+++ api/database/factories/OrderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'customer_id' => Customer::factory()->create()->id,
+            'total' => 0,
+            'status' => 'open',
+            'paid_at' => null,
+        ];
+    }
+}
